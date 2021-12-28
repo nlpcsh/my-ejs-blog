3764e28d5caebaa38c1fe14e7406886bc7e3a2ff
Nikolay Petrov <nikolay.petrov@abv.bg>
2021-12-28 12:48:47 +0200

    create home page

--- app.js
+++ app.js
@@ -14,6 +14,9 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
+app.get('/', function (req, res) {
+    res.render('home', { homeInfo: homeStartingContent });
+});
 
 
 
@@ -26,7 +29,6 @@ app.use(express.static('public'));
 
 
 
-
-app.listen(3000, function () {
-    console.log('Server started on port 3000');
+app.listen(3003, function () {
+    console.log('Server started on port 3003');
 });
